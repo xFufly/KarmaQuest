bfbb059ad71aa61fef08302074dea4efbe688cb9
CaraPloof <caraploof@gmail.com>
2025-01-19 10:08:19 +0100

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,9 @@
+
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+}
